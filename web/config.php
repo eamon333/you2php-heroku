8fa51f0feac0f1c6acf5d3b651d50a5e1f8ec87e
eamon333 <35907472+eamon333@users.noreply.github.com>
2018-01-29 20:22:27 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
+define('GJ_CODE', getenv("US"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
