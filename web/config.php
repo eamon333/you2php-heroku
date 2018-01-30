02e40467cbcabd083eedd1182c603a72af367330
eamon333 <35907472+eamon333@users.noreply.github.com>
2018-01-30 09:09:22 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("US"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyCZ-W-45M3vIycxOnCA4jfWv5Rb4FpB6q4');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'Youtube');//网站名字
+define('TITLENAME', 'Youtube');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'eamon0822@hotmail.com');//邮箱
 ?>
